36672522bd8e081f51a14aac4e1cf9714cb42416
Gabrijel <gabrijel.gordic@gmail.com>
2025-12-09 15:33:56 +0100

    implement profile model and auto-creation signals

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(
+        upload_to='avatars/', default='avatars/default.jpg', blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    phone_number = models.CharField(max_length=20, blank=True)
+    is_verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
